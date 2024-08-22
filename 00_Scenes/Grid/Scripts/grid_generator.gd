cf1349a2a2c7409d9c1fdc7a44e6d73a57199347
Natwm <natw.mezza@hotmail.fr>
2024-08-22 17:52:03 +0200

    NMezza - [Add] Base Call for IA

--- 00_Scenes/Grid/Scripts/grid_generator.gd
+++ 00_Scenes/Grid/Scripts/grid_generator.gd
@@ -1,5 +1,6 @@
 @tool
 extends Node3D
+class_name GameboardCreator
 
 ### Export ###
 @export_category("Resources")
@@ -16,7 +17,7 @@ extends Node3D
 ### ready ###
 var analyse_element : Array
 var space_state : PhysicsDirectSpaceState3D
-var gameboard : Array
+var gameboard : Dictionary
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,6 +30,7 @@ func _ready():
 func _process(delta):
 	if(grid_param):
 		DebugDraw3D.draw_box(global_position,Quaternion.IDENTITY,Vector3(grid_param.grid_size.x,.25,grid_param.grid_size.y),Color.GREEN)
+		
 		#for x in range(grid_param.grid_size.x):
 			#for y in range(grid_param.grid_size.y):
 				#DebugDraw3D.draw_cylinder_ab(Vector3(0.5+x,2,0.5+y),Vector3(0.5+x,0,0.5+y),0.25,Color.RED,1000)
@@ -38,7 +40,6 @@ func _process(delta):
 func Create_Analyse_board():
 	for x in range(grid_param.grid_size.x):
 		for y in range(grid_param.grid_size.y):
-			
 			var current_analyse_element : PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(
 				Vector3(x + grid_param._half_cell_size,2,y + grid_param._half_cell_size),
 				Vector3(x + grid_param._half_cell_size,-1,y + grid_param._half_cell_size)) 
@@ -46,8 +47,7 @@ func Create_Analyse_board():
 			var result :Dictionary = space_state.intersect_ray(current_analyse_element)
 			if(!result.is_empty()):
 				print("position = ("+ str(x) + "," + str(y) + ") = Result " + result.collider.name )
-				DebugDraw3D.draw_cylinder_ab(Vector3(0.5+x,2,0.5+y),Vector3(0.5+x,0,0.5+y),0.25,Color.RED,1000)
-				gameboard.append(create_tile(Vector3(x,0,y)))
+				gameboard[Vector3(x,0,y)] = create_tile(Vector3(x,0,y))
 			pass
 		pass
 	pass
